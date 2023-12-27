0dcc40a787faa4040989d7388cc80e42c7760d2e
unknown <sahilshah2104@gmail.com>
2023-12-27 23:31:26 +0530

    Added Problem

---
+++ CodeForces ProblemSet/630A Again Twenty Five.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve() {
+    int n;
+    cin >> n; 
+    cout << 25 << endl;
+}
+
+int main() {
+    solve();
+}
+
+// Since whatever number we increase 5 exponentially all the numbers creates will have 25 as their last 2 digits
